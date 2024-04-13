e71ec721b247687eb4230a9d8d06350472a187d5
DmPrkp <DmPrkp.mail@gmail.com>
2024-04-13 23:03:10 +0300

    fix static server

--- ionic-client/static-server.go
+++ ionic-client/static-server.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./build"))
-	http.Handle("/", fs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./build/index.html")
+	})
+
 
 	log.Print("Listening on :5173...")
 	err := http.ListenAndServe(":5173", nil)
--- shared-types/entity/image.d.ts
+++
@@ -1,8 +0,0 @@
-type ImageType = {
-  src: string;
-  alt?: string;
-  width?: number;
-  height?: number;
-};
-
-export { ImageType };
