5067f004abbbb938a02649e7570771cc4d413267
DmPrkp <DmPrkp.mail@gmail.com>
2024-12-24 21:07:25 +0300

    fix migration

--- order-server/prisma/migrations/20241218193524_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Zayavka" (
-    "uuid" TEXT NOT NULL,
-    "data" JSONB NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Zayavka_uuid_key" ON "Zayavka"("uuid");
---
+++ order-server/prisma/migrations/20241224171037_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Zayavka" (
+    "id" SERIAL NOT NULL,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Zayavka_pkey" PRIMARY KEY ("id")
+);
