8030ad96a42cece7639d26355d18835af86a1bc1
DmPrkp <DmPrkp.mail@gmail.com>
2025-02-05 23:33:18 +0300

    fix mode

--- ionic-client/src/components/logicalSwitchers/ThemeSwitch.vue
+++ ionic-client/src/components/logicalSwitchers/ThemeSwitch.vue
@@ -8,18 +8,35 @@
   import { IonIcon } from "@ionic/vue";
   import { sunnyOutline, moonOutline } from "ionicons/icons";
   import { onBeforeMount, ref } from "vue";
+  import Cookies from "js-cookie";
 
   const isDarkMode = ref(false);
+  const MODE = {
+    dark: "dark",
+    light: "light",
+  };
 
   const toggleTheme = (value: boolean | undefined) => {
     if (typeof value !== "boolean") value = !isDarkMode.value;
     isDarkMode.value = value;
-    document.body.classList.toggle("dark", isDarkMode.value);
+    document.body.classList.toggle(MODE.dark, isDarkMode.value);
+    Cookies.set("theme_mode", value ? MODE.dark : MODE.light, { expires: 365 });
   };
 
   onBeforeMount(() => {
+    checkMode();
+  });
+
+  function checkMode() {
+    const mode = Cookies.get("theme_mode");
+
+    if (mode) {
+      toggleTheme(mode === MODE.dark);
+      return;
+    }
+
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       toggleTheme(true);
     }
-  });
+  }
 </script>
