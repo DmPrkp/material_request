7eedb8461e64aee8725ec39973494f149535a60f
DmPrkp <DmPrkp.mail@gmail.com>
2025-03-09 17:13:32 +0300

    unblock skaffold link

--- ionic-client/src/constants/systems/index.ts
+++ ionic-client/src/constants/systems/index.ts
@@ -1,32 +1,36 @@
 export const FLAT_ROOF_SYSTEM = {
   id: 1,
-  title: 'flat',
-  description: 'flat roofs',
+  title: "flat",
+  description: "flat roofs",
   img: {
-    src: '/system/flat-roof.jpg',
-    alt: 'flat-roof',
+    src: "/system/flat-roof.jpg",
+    alt: "flat-roof",
   },
 } as const;
 
 export const EIFS_FACADE_SYSTEM = {
   id: 2,
-  title: 'EIFS',
-  description: 'Exterior Insulation Finishing System',
+  title: "EIFS",
+  description: "Exterior Insulation Finishing System",
   img: {
-    src: '/system/wet-facade-narrow.jpg',
-    alt: 'wet-facade',
+    src: "/system/wet-facade-narrow.jpg",
+    alt: "wet-facade",
   },
 } as const;
 
 export const FRAME_SCAFFOLD_SYSTEM = {
   id: 2,
-  title: 'frame scaffold',
-  description: 'frame scaffold',
+  title: "frame scaffold",
+  description: "frame scaffold",
   img: {
-    src: '/system/scaffold.jpg',
-    alt: 'scaffold',
+    src: "/system/scaffold.jpg",
+    alt: "scaffold",
   },
-  disable: true,
+  disable: false,
 } as const;
 
-export const SYSTEMS = [EIFS_FACADE_SYSTEM, FLAT_ROOF_SYSTEM, FRAME_SCAFFOLD_SYSTEM];
+export const SYSTEMS = [
+  EIFS_FACADE_SYSTEM,
+  FLAT_ROOF_SYSTEM,
+  FRAME_SCAFFOLD_SYSTEM,
+];
