005efe0f3c6cae46bac971ed8ed505776b7482fd
DmPrkp <DmPrkp.mail@gmail.com>
2025-09-22 00:25:58 +0300

    add console log

--- order-server/prisma/prisma.service.ts
+++ order-server/prisma/prisma.service.ts
@@ -4,9 +4,11 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+  implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
+    console.log('DB HOST', process.env.DB_USER);
+    console.log('DB HOST', process.env.DB_PASSWORD);
+    console.log('DB HOST', process.env.POSTGRES_DB);
     await this.$connect();
   }
 
